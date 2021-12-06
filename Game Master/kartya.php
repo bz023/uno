111a280390218f36acd47654b3646b1690ab0a0d
bz023 <biro.zoltan023@gmail.com>
2021-12-06 15:03:29 +0100

    mukodik a frissites yayyyy

--- Game Master/kartya.php
+++ Game Master/kartya.php
@@ -17,20 +17,19 @@
         die("Connection failed: " . mysqli_connect_error());
         }
 
-        $sql = "SELECT kartyak.kep FROM kartyak INNER JOIN jatek ON kartyak.id = jatek.kartyaid";
+        $sql = "SELECT kartyak.kep FROM kartyak INNER JOIN jatek ON kartyak.id = jatek.kartyaid WHERE jatek.playerid=0";
         $result = mysqli_query($conn, $sql);
 
         if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-            echo "jó";
+            echo '<img src="..'.$row["kep"].'">';
         }
         } else {
-        echo "nemjó";
+        echo "valami nemjau";
         }
 
         mysqli_close($conn);
     ?>
-        <img src="../kartyak/Blue_5.png">
     </body>
 </html>
